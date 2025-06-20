512440300dafba46fd7bcb2f627dab5dfc943578
Mayur Jain <mayurjain333@gmail.com>
2025-06-20 16:19:09 -0700

    google analytics

---
+++ js/google-analytics.js
@@ -0,0 +1,17 @@
+(function(){
+  const GA_MEASUREMENT_ID = 'G-XXXXXXXXXX';
+
+  // Load the gtag.js script dynamically
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script);
+
+  // Initialize dataLayer and gtag
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', GA_MEASUREMENT_ID);
+})();
